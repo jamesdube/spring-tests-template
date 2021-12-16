37a1936d477aa8f52f0627a5ecc41f22f49b26fd
James Dube <jamesdylandube@gmail.com>
2021-12-16 06:11:56 +0200

    init

---
+++ src/main/java/com/jamesdube/tests/template/TestsTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.jamesdube.tests.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamesdube/tests/template/TestsTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesdube.tests.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
